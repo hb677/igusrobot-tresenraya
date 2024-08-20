121e0ed46677b6f458aeb2caccbdadf2b81b13c3
henryitq <gladstone@stetter-itq.com>
2024-08-20 12:25:32 +0100

    other part of send alive jog whilst waiting

--- Codigo 09.08.24/igus_robot.py
+++ Codigo 09.08.24/igus_robot.py
@@ -43,3 +43,31 @@ def sendMessageToRobot(message):
         sock.sendall(arrayAliveJog)
         time.sleep(0.1)			
 
+def sendNoPrint():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Enter the IP address of the robot (192.168.3.11) here if you're not using a CPRog/iRC simulation
+    server_address = ('192.168.3.11', 3920)
+    sock.connect(server_address)
+
+    # The ALIVEJOG message needs to be sent regularly (at least once a second) to keep the connection alive
+    messageAliveJog = "CRISTART 1234 ALIVEJOG 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 CRIEND"
+
+
+    # Encode the messages
+    encodedAliveJog=messageAliveJog.encode('utf-8')
+    arrayAliveJog=bytearray(encodedAliveJog)
+        
+
+    # Send first ALIVEJOG to establish the connection
+    sock.sendall(arrayAliveJog)
+    time.sleep(0.1)
+
+    # I'm sending 10 more ALIVEJOG messages to keep the connection alive.
+    # If I drop the connection too early our message may not get through.
+    # A production program should send this once or twice a second from a parallel thread.
+    for i in range (1, 10):
+        sock.sendall(arrayAliveJog)
+        time.sleep(0.1)			
+
