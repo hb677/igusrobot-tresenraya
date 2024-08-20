74b51534e78fd95e8e6745a41144bef30e801df7
henryitq <gladstone@stetter-itq.com>
2024-08-20 12:07:12 +0100

    Removed sock close from send message

--- Codigo 09.08.24/igus_robot.py
+++ Codigo 09.08.24/igus_robot.py
@@ -11,39 +11,35 @@ def sendMessageToRobot(message):
     sock.connect(server_address)
     print("Connected")
 
-    try:
     # The ALIVEJOG message needs to be sent regularly (at least once a second) to keep the connection alive
-        messageAliveJog = "CRISTART 1234 ALIVEJOG 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 CRIEND"
+    messageAliveJog = "CRISTART 1234 ALIVEJOG 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 CRIEND"
     # This is my intended message (I use CMD DOUT since this creates a log entry on success)
-        message = "CRISTART 1234 CMD " + message + " CRIEND"
+    message = "CRISTART 1234 CMD " + message + " CRIEND"
 
 
     # Encode the messages
-        encodedAliveJog=messageAliveJog.encode('utf-8')
-        encoded=message.encode('utf-8')
-        arrayAliveJog=bytearray(encodedAliveJog)
-        array=bytearray(encoded)
+    encodedAliveJog=messageAliveJog.encode('utf-8')
+    encoded=message.encode('utf-8')
+    arrayAliveJog=bytearray(encodedAliveJog)
+    array=bytearray(encoded)
         
 
     # Send first ALIVEJOG to establish the connection
-        print("Sending ALIVEJOG")
-        sock.sendall(arrayAliveJog)
-        time.sleep(0.1)
+    print("Sending ALIVEJOG")
+    sock.sendall(arrayAliveJog)
+    time.sleep(0.1)
         
     # Send the main message
-        print("Sending message")
-        sock.sendall(array)
+    print("Sending message")
+    sock.sendall(array)
         
 
     # I'm sending 10 more ALIVEJOG messages to keep the connection alive.
     # If I drop the connection too early our message may not get through.
     # A production program should send this once or twice a second from a parallel thread.
-        print("Keeping connection alive")
-        for i in range (1, 10):
-            print("Sending ALIVEJOG")
-            sock.sendall(arrayAliveJog)
-            time.sleep(0.1)			
+    print("Keeping connection alive")
+    for i in range (1, 10):
+        print("Sending ALIVEJOG")
+        sock.sendall(arrayAliveJog)
+        time.sleep(0.1)			
 
-    finally:
-        print("Finally")
-        sock.close()
